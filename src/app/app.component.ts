11cebfc5cf29ea3285b158585be9e7167e4d25a9
wep1980 <wepbike@gmail.com>
2021-01-13 12:54:42 -0300

    Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms'; // FormGroup -> Classe que apresenta os campos do formulario
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  mensagem: string = 'Olá mundo';
+ 
+  // form -> variavel utilizada no form da pagina HTML
+  form : FormGroup = new FormGroup({
+
+    // description -> Variavel igual a criada na APi e que serve como referencia na pagina HTML para capturar a descrição digitada na tela.
+    description : new FormControl('') // O valor inicial e uma String vazia, FormControl-> Objeto JavaScript.
+  })
+
+  submit(){
+    console.log(this.form.value)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ReactiveFormsModule } from '@angular/forms'; // Importe para trabalhar com formularios
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
